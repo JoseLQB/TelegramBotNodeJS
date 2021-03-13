f72eed81c4ef37934150821e1792a664443f71fb
JoseLQB <josequb88@gmail.com>
2021-03-13 02:41:20 +0100

    second commit

--- #1_Test/bot.js
+++ #1_Test/bot.js
@@ -3,7 +3,29 @@ const {Telegraf} = require('telegraf');
 const bot = new Telegraf('1622090153:AAE_6qEsPdE3iN-gbTjjfxd2cMa_iUnyiNQ');
 
 bot.start((ctx)=>{
-    ctx.reply('TDS PTS');
+    ctx.reply(`/help para ayuda 
+/settings para configuración
+/saludo para saludar`);
 });
+
+bot.help((ctx)=>{
+    ctx.reply('No tengo mucha ayuda que darte.');
+});
+
+bot.settings((ctx)=>{
+    ctx.reply('Soy un bot muy sencillo, solo me hicieron para aprender. No necesitas saber mucha cosa.');
+});
+
+bot.command(['saludo', 'SALUDO', 'Saludo'], (ctx)=>{
+    ctx.reply('¡¡Hola!!')
+})
+
+bot.hears(['tds', 'TDS'], (ctx)=>{
+    ctx.reply("TDS PTS");
+})
+
+bot.on("text", (ctx)=>{
+    ctx.reply("Has escrito, felicidades, sabes escribir");
+})
 
 bot.launch();
